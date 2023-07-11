2fe2be26d1cbd4bb09553c262cab762d2ac3da3f
Mateusvr <mateusvr2005@outlook.com>
2023-07-10 21:48:50 -0300

    aula bd 10-07

--- src/server.js
+++ src/server.js
@@ -1,10 +1,31 @@
-const express = require('express')
+//const express = require('express')
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+
 const app = express()
+
 const HOST = 'localhost'
 const port = 5000
 
+//endpoint inicial
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Servidor rodando!')
+})
+
+const DB_USER= process.env.DB_USER
+const DB_PASS= process.env.DB_PASS
+
+console.log(DB_USER, DB_PASS);
+
+//conectando no banco Mongo Atlas
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.vusmgxh.mongodb.net/usuario?retryWrites=true&w=majority`)
+.then(() =>{
+    console.log('BD conectado com sucesso!');
+})
+.catch(error => {
+    console.log('Erro ao conectar banco de dados.', error);
 })
 
 app.listen(port, () => {
